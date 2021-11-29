20a7a884aa1625281dbfc8425518a84cdb35a739
Danny Kirchmeier <danny@kirchmeier.us>
2021-11-29 09:13:16 -0800

    Day 02

--- 02/index.ts
+++ 02/index.ts
@@ -1,23 +1,51 @@
 import * as assert from 'assert';
-import { readFileSync } from 'fs';
+import {readFileSync} from 'fs';
 import _ = require('lodash');
 
 function main() {
+  const example = `\
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`;
   const input = _.trim(readFileSync(__dirname + '/input.txt', 'utf8'));
 
-  assert.strictEqual(null, solveA(""));
-  assert.strictEqual(null, solveA(input));
+  assert.strictEqual(2, solveA(example));
+  assert.strictEqual(586, solveA(input));
 
-  assert.strictEqual(null, solveB(""));
-  assert.strictEqual(null, solveB(input));
+  assert.strictEqual(1, solveB(example));
+  assert.strictEqual(352, solveB(input));
 }
 
 main();
 
-function solveA(input: string) {
-  return input.length;
+function parseInput(input: string){
+  return input.split('\n').map(it=>{
+    const [, a, b, char, pass] = it.match(/(\d+)-(\d+) (\w): (.+)/)!;
+    return {
+      char, pass,
+      a: Number.parseInt(a, 10),
+      b: Number.parseInt(b, 10),
+    }
+  })
 }
 
-function solveB(input: string) {
-  return input.length;
+function solveA(rawInput: string) {
+  return _.sumBy(
+    parseInput(rawInput),
+    it=> {
+      const charCount = _.sumBy(it.pass, c=> c === it.char ? 1 : 0)
+      return it.a <= charCount && charCount <= it.b ? 1 : 0
+    }
+  )
+}
+
+function solveB(rawInput: string) {
+  return _.sumBy(
+    parseInput(rawInput),
+    it => {
+      const a = it.pass[it.a - 1] === it.char;
+      const b = it.pass[it.b - 1] === it.char;
+      return a !== b ? 1 : 0;
+    }
+  )
 }
