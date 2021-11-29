988125bd66991a49ffe4b9ae7d6748bf696bbe16
Danny Kirchmeier <danny@kirchmeier.us>
2021-11-29 09:21:34 -0800

    Day 03

--- 03/index.ts
+++ 03/index.ts
@@ -4,20 +4,51 @@ import _ = require('lodash');
 
 function main() {
   const input = _.trim(readFileSync(__dirname + '/input.txt', 'utf8'));
+  const example = `\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`;
 
-  assert.strictEqual(null, solveA(""));
-  assert.strictEqual(null, solveA(input));
+  assert.strictEqual(7, solveA(example));
+  assert.strictEqual(247, solveA(input));
 
-  assert.strictEqual(null, solveB(""));
-  assert.strictEqual(null, solveB(input));
+  assert.strictEqual(336, solveB(example));
+  assert.strictEqual(2983070376, solveB(input));
 }
 
 main();
 
+function calcCollisions(lines: string[], right: number, down: number){
+  let trees = 0;
+  let x = right;
+  for(let i = down; i < lines.length; i+= down){
+    const ln = lines[i];
+    if(ln[x % ln.length] === '#'){
+      trees +=1;
+    }
+    x += right
+  }
+  return trees;
+}
+
 function solveA(input: string) {
-  return input.length;
+  const lines = input.split('\n');
+  return calcCollisions(lines, 3, 1);
 }
 
 function solveB(input: string) {
-  return input.length;
+  const lines = input.split('\n');
+  return calcCollisions(lines, 1, 1) *
+    calcCollisions(lines, 3, 1) *
+    calcCollisions(lines, 5, 1) *
+    calcCollisions(lines, 7, 1) *
+    calcCollisions(lines, 1, 2);
 }
